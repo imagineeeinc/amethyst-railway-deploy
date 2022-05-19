7ea2c9747ad205b8537c7a698b9bc3e9b92e8b52
Imagineee.inc <imagineeelego@gmail.com>
2022-05-19 19:12:07 +0000

    added npm start

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var exec = require('child_process').exec;
 
-exec('git clone https://github.com/imagineeeinc/Amethyst.git && cd Amethyst && npm i', function (error, stdout, stderr) {
+exec('git clone https://github.com/imagineeeinc/Amethyst.git && cd Amethyst && npm i && npm start ', function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
     if (error !== null) {
