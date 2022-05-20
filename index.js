bc7af2a36b450bc359868c3a9c62de7dc8ec27ab
Imagineee <imagineeelego@gmail.com>
2022-05-20 07:57:49 +0100

    added g++

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var exec = require('child_process').exec;
 
-exec('git clone https://github.com/imagineeeinc/Amethyst.git && cd Amethyst && npm i && npm start ', function (error, stdout, stderr) {
+exec('apt install g++ && git clone https://github.com/imagineeeinc/Amethyst.git && cd Amethyst && npm i && npm start ', function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
     if (error !== null) {
