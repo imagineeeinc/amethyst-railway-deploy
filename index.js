adb032dea523b05da61b35f027818e3fede220df
Imagineee <imagineeelego@gmail.com>
2022-05-21 19:24:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var exec = require('child_process').exec;
 
-exec("echo 'deb http://ftp.us.debian.org/debian stretch main contrib non-free' >> /etc/apt/sources.list && apt-get update && apt install ffmpeg && git clone https://github.com/imagineeeinc/Amethyst.git && cd Amethyst && npm i && npm start!", function (error, stdout, stderr) {
+exec("echo 'deb http://ftp.us.debian.org/debian stretch main contrib non-free' >> /etc/apt/sources.list && apt-get update && apt install ffmpeg", function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+            console.log('exec error: ' + error);
+    }
+});
+exec("git clone https://github.com/imagineeeinc/Amethyst.git && cd Amethyst && npm i && npm start", function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
     if (error !== null) {
