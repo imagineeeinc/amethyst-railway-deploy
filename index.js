d23cef0771853d9cd2ef71a11542d4c216db3fd3
Imagineee <imagineeelego@gmail.com>
2022-05-24 16:14:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var exec = require('child_process').exec;
 
-exec("curl https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -L -o ffmpeg.tar.gz && tar -xf ffmpeg.tar.gz -C ~/bin && echo 'export PATH=$PATH:bin/ffmpeg-5.0.1-amd64-static' >> ~/.bashrc", function (error, stdout, stderr) {
+exec("curl https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -L -o ffmpeg.tar.gz && mkdir ~/bin && tar -xf ffmpeg.tar.gz -C ~/bin && echo 'export PATH=$PATH:bin/ffmpeg-5.0.1-amd64-static' >> ~/.bashrc", function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
     if (error !== null) {
